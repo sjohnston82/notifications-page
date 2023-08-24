32a00893c1774ebb045851040a509f19bc228aee
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-08-24 09:39:03 -0400

    adding nanostore for state

--- src/components/NotificationsBar.tsx
+++ src/components/NotificationsBar.tsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { $unreadLength } from "../store/notifications";
+import { useStore } from "@nanostores/react";
 
 const NotificationsBar = () => {
-  const [unread, setUnread] = useState(false)
-  return (
-    <div className="flex justify-between font-plusjakartasans mt-6">
-      <div className="flex gap-2">
-        <h1 className="font-bold">Notifications</h1>
-        <div className="bg-primary-blue text-white px-3 py1 rounded-lg">3</div>
-      </div>
-      <div className=""><p className="text-dark-grayish-blue">Mark all as read</p></div>
-    </div>
-  )
-}
+  const [unread, setUnread] = useState(false);
+  const unreadLength = useStore($unreadLength);
 
-export default NotificationsBar
+
+  return (
+    <div className="flex justify-between font-plusjakartasans mt-6 mb-8">
+      <div className="flex gap-2">
+        <h1 className="font-[800] text-[18px]">Notifications</h1>
+        <div className="bg-primary-blue text-white px-3 py1 rounded-lg">{unreadLength.length}</div>
+      </div>
+      <div className="">
+        <p className="text-dark-grayish-blue">Mark all as read</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationsBar;
---
+++ src/store/notifications.ts
@@ -0,0 +1,105 @@
+import { atom, computed } from "nanostores";
+
+type Notification = {
+  name: string;
+  type:
+    | "post-reaction"
+    | "follow"
+    | "joined"
+    | "left"
+    | "message"
+    | "picture-comment";
+  avatar: string;
+  message?: string;
+  time: string;
+  postName?: string;
+  commentedPicture?: string;
+  unread: boolean;
+};
+
+export const notification = atom<Notification>({
+  name: "",
+  type: "post-reaction",
+  avatar: "",
+  time: "",
+  postName: "",
+  commentedPicture: "",
+  message: "",
+  unread: false
+});
+
+export const $notifications = atom<Notification[]>([
+  {
+    name: "Mark Webber",
+    type: "post-reaction",
+    avatar: "/avatar-mark-webber.webp",
+    message: "",
+    time: "1m ago",
+    postName: "My first tournament today!",
+    commentedPicture: "",
+    unread: true,
+  },
+  {
+    name: "Angela Gray",
+    type: "follow",
+    avatar: "/avatar-angela-gray.webp",
+    message: "",
+    time: "5m ago",
+    postName: "",
+    commentedPicture: "",
+    unread: true,
+  },
+  {
+    name: "Jacob Thompson",
+    type: "joined",
+    avatar: "/avatar-jacob-thompson.webp",
+    message: "",
+    time: "1 day ago",
+    postName: "",
+    commentedPicture: "",
+    unread: true,
+  },
+  {
+    name: "Rizky Hasanuddin",
+    type: "message",
+    avatar: "/avatar-rizky-hasanuddin.webp",
+    message:
+      "Hello, thanks for setting up the Chess Club.  I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+    time: "5 days ago",
+    postName: "",
+    commentedPicture: "",
+    unread: false,
+  },
+  {
+    name: "Kimberly Smith",
+    type: "picture-comment",
+    avatar: "/avatar-kimberly-smith.webp",
+    message: "",
+    time: "1 week ago",
+    postName: "",
+    commentedPicture: "/image-chess.webp",
+    unread: false,
+  },
+  {
+    name: "Nathan Peterson",
+    type: "post-reaction",
+    avatar: "/avatar-nathan-peterson.webp",
+    message: "",
+    time: "2 weeks ago",
+    postName: "5 end-game strategies to increase your win rate",
+    commentedPicture: "",
+    unread: false,
+  },
+  {
+    name: "Anna Kim",
+    type: "left",
+    avatar: "/avatar-anna-kim.webp",
+    message: "",
+    time: "2 weeks ago",
+    postName: "",
+    commentedPicture: "",
+    unread: false,
+  },
+]);
+
+export const $unreadLength = computed($notifications, n => n.filter(i => i.unread))
